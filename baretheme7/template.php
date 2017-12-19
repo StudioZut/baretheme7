7516a1c1ac8d12d777811349e122783dea4ef368
Christian Aldridge <Studiozut@gmail.com>
2017-12-19 12:12:18 -0500

    Set themename var

--- baretheme7/template.php
+++ baretheme7/template.php
@@ -1,5 +1,8 @@
+// setting var for theme name since this is just a starter theme
+$themename = 'baretheme7';
+
 // call css for special node type
 if(isset($variables['node']->type && $variables['node']->type == "my-custom-node-type"){
-    drupal_add_css(drupal_get_path('theme', 'baretheme7').'/css/node-my-custom-node-type.css');
+    drupal_add_css(drupal_get_path('theme', $themename).'/css/node-my-custom-node-type.css');
 }
 // end
